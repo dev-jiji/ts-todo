2d77c03da5cac6f953e2e8da9137ff9cc4ee70b6
jieun <wldmsdlsns1@gmail.com>
2023-03-14 12:02:16 +0900

    TypeScript 에 redux-persist 적용하기

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,19 @@ import AppContainer from "./AppContainer";
 // redux 적용하기
 import { store } from "./store/store";
 import { Provider } from "react-redux";
+// persist 적용
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+export let persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
 root.render(
     <Provider store={store}>
-        <AppContainer />
+        <PersistGate loading={null} persistor={persistor}>
+            <AppContainer />
+        </PersistGate>
     </Provider>
 );
 
